d05bc70be1a272c534d8127ac0b0181e7935d9b3
Monique <mbihomonique19@yahoo.com>
2019-02-26 22:57:25 +0100

    ajout de ligne d'affichage

--- MonProjetGit/src/Main.java
+++ MonProjetGit/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ce projet est vide");
-
+        System.out.println("il sert juste de base pour apprendre Git");
 	}
 
 }
